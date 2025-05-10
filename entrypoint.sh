9b1203543b225734fac4399a2676fcf6a202020e
wowcrise54 <wowcrise54@gmail.com>
2025-05-10 09:24:54 +0000

    merge with front done

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,7 @@ until nc -z "$DB_HOST" "$DB_PORT"; do
 done
 
 echo "🚀 Running Alembic migrations…"
-<<<<<<< HEAD
-# python -m alembic init migrations
-# cp database/alembic.ini alembic.ini 
-python -m alembic -c alembic.ini upgrade head
-=======
 python -m alembic -c database/alembic.ini upgrade head
->>>>>>> parent of 36874fa (add valid, reg/auth)
 
 echo "🎉 Starting FastAPI"
 exec uvicorn database.router:app --host 0.0.0.0 --port 8000
